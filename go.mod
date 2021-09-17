e66045451265c3b17466a04f1d4e3d869b348059
Ahmad Izzan Zahrial <izzanzahrial@outlook.com>
2021-09-17 21:17:13 +0700

    update test go module, add parameter

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/izzanzahrial/learn-go-module
+module github.com/izzanzahrial/learn-go-module/v2 // <-- the v2 part is major change
 
 go 1.17
--- learn-go-module.go
+++ learn-go-module.go
@@ -1,5 +1,5 @@
 package learn_go_module
 
-func TestGoModule() string {
+func TestGoModule(name string) string {
 	return "This is test module version 1.0.1"
 }
